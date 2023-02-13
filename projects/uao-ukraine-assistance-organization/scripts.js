45c2629021b98c02613785394137a788f2e6ec23
Gabriel Vazquez <gavf.vazquez@gmail.com>
2023-02-13 18:24:35 -0300

    Revert "Make stories, add header, description and carousel"
    This reverts commit 9bb750fe8a57461d0f0bf4374041f831a3c5a2ae.

--- projects/uao-ukraine-assistance-organization/scripts.js
+++ projects/uao-ukraine-assistance-organization/scripts.js
@@ -1,20 +1,15 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 0) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
-  });
-
-  // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-});
-
-// Section carousel
-var mySwiper = new Swiper('.mySwiper', {
-    initialSlide: 1, // iniciar en el segundo slide
+    $(window).scroll(function () {
+        if (this.scrollY > 0) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
     });
+
+    // toggle menu/navbar script
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+});
